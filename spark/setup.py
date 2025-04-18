a52ed11d81feb7ba0b076c2caf524126d0b1b099
vinay-ram1999 <vinayramgazula@gmail.com>
2025-04-18 03:56:21 -0400

    setup working;

--- spark/setup.py
+++ spark/setup.py
@@ -4,30 +4,32 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType, 
 import os
 
 ## DEFINE SENSITIVE VARIABLES
-CATALOG_URI = "http://nessie:19120/api/v1"  # Nessie Server URI
-WAREHOUSE = "s3://warehouse/"               # Minio Address to Write to
-STORAGE_URI = "http://172.18.0.2:9000"      # Minio IP address from docker inspect
+NESSIE_URI = os.environ.get("NESSIE_URI") ## Nessie Server URI
+WAREHOUSE = os.environ.get("WAREHOUSE") ## BUCKET TO WRITE DATA TOO
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY") ## AWS CREDENTIALS
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY") ## AWS CREDENTIALS
+AWS_S3_ENDPOINT= os.environ.get("AWS_S3_ENDPOINT") ## MINIO ENDPOINT
 
 # Configure Spark with necessary packages and Iceberg/Nessie settings
 conf = (
     pyspark.SparkConf()
-        .setAppName('sales_data_app')
-        # Include necessary packages
-        .set('spark.jars.packages', 'org.postgresql:postgresql:42.7.3,org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.0,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.5_2.12:0.77.1,software.amazon.awssdk:bundle:2.24.8,software.amazon.awssdk:url-connection-client:2.24.8')
-        # Enable Iceberg and Nessie extensions
+        .setAppName('app_name')
+        .set('spark.jars.packages', 'org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.3.1,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.3_2.12:0.67.0,software.amazon.awssdk:bundle:2.17.178,software.amazon.awssdk:url-connection-client:2.17.178')
         .set('spark.sql.extensions', 'org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions,org.projectnessie.spark.extensions.NessieSparkSessionExtensions')
-        # Configure Nessie catalog
         .set('spark.sql.catalog.nessie', 'org.apache.iceberg.spark.SparkCatalog')
-        .set('spark.sql.catalog.nessie.uri', CATALOG_URI)
+        .set('spark.sql.catalog.nessie.uri', NESSIE_URI)
         .set('spark.sql.catalog.nessie.ref', 'main')
         .set('spark.sql.catalog.nessie.authentication.type', 'NONE')
         .set('spark.sql.catalog.nessie.catalog-impl', 'org.apache.iceberg.nessie.NessieCatalog')
-        # Set Minio as the S3 endpoint for Iceberg storage
-        .set('spark.sql.catalog.nessie.s3.endpoint', STORAGE_URI)
+        .set('spark.sql.catalog.nessie.s3.endpoint', AWS_S3_ENDPOINT)
         .set('spark.sql.catalog.nessie.warehouse', WAREHOUSE)
         .set('spark.sql.catalog.nessie.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO')
+        .set('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY)
+        .set('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_KEY)
 )
 
-# Start Spark session
+
+## Start Spark Session
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
-print("Spark Session Started")
+# print("Spark Running")
+
